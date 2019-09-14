acfc99323e36339e4521bc7a364f6c6b3759179b
mahisa agnu <54902186+mahisa23-siks@users.noreply.github.com>
2019-09-14 18:58:40 +0700

    Add files via upload

---
+++ botspam/index.sh
@@ -0,0 +1,92 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+sleep 1
+echo "\033[33;1m Nyepam gass pool"
+echo "\033[33;1m FACEBOOK : mahisa23"
+echo "\033[33;1m INSTAGRAM : mahisa23"
+echo "\033[33;1m WEBSITE : www.siksonline.com and www.hokilagi.com"
+sleep 1
+echo "\033[36;1m★""\033[34;1m@mahisa23""\033[36;1m★"
+sleep 1
+echo "\033[32;1m Nyepam Anu Aya :"
+echo "\033[35;1m1.""\033[36;1m BUKALAPAK"
+echo "\033[35;1m2.""\033[36;1m TELKOMSEL"
+echo "\033[35;1m3.""\033[36;1m GRAB"
+echo "\033[35;1m4.""\033[36;1m TOKOPEDIA"
+echo "\033[31;1m0. Mulang atau Uih"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[31;1mJenis Spam:""\033[37;1mChat WhatsApp"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1m@mahisa23"
+sleep 5
+clear
+cd $HOME/botspam/Tul/
+php 1
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1m@mahisa23"
+sleep 5
+clear
+cd $HOME/botspam/Tul/
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Grab"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1m@mahisa23"
+sleep 5
+clear
+cd $HOME/botspam/Tul/
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono9 -F gay "Toked"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1m1 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1m@mahisa23"
+sleep 5
+clear
+cd $HOME/botspam/Tul/
+php 4.php
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Mulang wa"
+sleep 1
+echo "\033[32;1m Hatur Nuhun Wilujeng Tepang Deui :)"
+sleep 1
+fi
+
